b034ad60e8667889522f52eda850beb1569087c2
Dejean.Sypher.26 <Dejean.Sypher26@cmc.edu>
2023-05-03 10:11:16 -0700

    Progress

--- run_maps.sh
+++ run_maps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-for file in '/data/Twitter dataset/'geoTwitter20*; do
-    ./src/map.py --input_path="$file" &
+#!/bin/sh
+for file in '/data/Twitter dataset/'geoTwitter20-*.zip; do
+	$(./src/map.py --input_path="$file") &
 done
--- src/map.py
+++ src/map.py
@@ -61,18 +61,20 @@ with zipfile.ZipFile(args.input_path) as archive:
                 # search hashtags
                 for hashtag in hashtags:
                     lang = tweet['lang']
-                    try:
-                        country = tweet['place']['country_code']
-                    except:
-                        country = 'UNK'
-                    if country == '':
-                        country = 'UNK'
+                    if tweet['place']:
+                        if tweet['place']['country_code']:
+                            country = tweet['place']['country_code']
+                        else:
+                            country = None
+                    else:
+                        country = None
                     if hashtag in text:
                         counter_lang[hashtag][lang] += 1
                         counter_country[hashtag][country] += 1
-
-                    counter_country['_all'][country] += 1
                     counter_lang['_all'][lang] += 1
+                    counter_country['_all'][country] += 1
+
+
 # open the outputfile
 try:
     os.makedirs(args.output_folder)
@@ -81,9 +83,11 @@ except FileExistsError:
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
-output_path_country= output_path_base+'.country'
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
     f.write(json.dumps(counter_lang))
+
+output_path_country = output_path_base+'.country'
+print('saving',output_path_country)
 with open(output_path_country,'w') as f:
     f.write(json.dumps(counter_country))
