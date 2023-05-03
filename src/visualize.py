af48cac746888ebedc80b510fb5dce106c4ab622
Dejean.Sypher.26 <Dejean.Sypher26@cmc.edu>
2023-05-03 14:25:16 -0700

    trial

--- src/visualize.py
+++ src/visualize.py
@@ -11,6 +11,10 @@ args = parser.parse_args()
 # imports
 import os
 import json
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 from collections import Counter,defaultdict
 
 # open the input path
@@ -24,5 +28,19 @@ if args.percent:
 
 # print the count values
 items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
-for k,v in items:
-    print(k,':',v)
+
+to_plot = items[:10]
+keys = [x[0] for x in to_plot][::-1]
+values = [y[1] for y in to_plot][::-1]
+
+plt.bar(range(len(keys)), values)
+plt.ylabel('Total Tweets')
+plt.xticks(range(len(keys)), keys)
+
+if 'lang' in args.input_path:
+    plt.xlabel('Language')
+    plt.savefig(f'{args.key}_(lang).png')
+else:
+    plt.xlabel('Country')
+    plt.savefig(f'{args.key}_(country).png')
+
